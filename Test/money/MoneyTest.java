c2e9e59a4efae28c63371316c6219f0c9956f2ca
10072002su_changping@cn.tre-inc.com <10072002su_changping@cn.tre-inc.com>
2021-05-17 23:48:36 +0900

    Initial commit

---
+++ Test/money/MoneyTest.java
@@ -0,0 +1,38 @@
+package money;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MoneyTest {
+
+    @Test
+    public void testMultiplication() {
+        Money five = Money.dollar(5);
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
+    }
+    @Test
+    public void testEquality() {
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+        assertFalse(Money.franc(5).equals(Money.franc(6)));
+        assertFalse(Money.franc(5).equals(Money.franc(6)));
+        assertFalse(Money.franc(5).equals(Money.dollar(6)));
+    }
+    @Test
+    public void testFrancMutiplication() {
+        Money fr = Money.franc(5);
+        assertEquals(Money.franc(10), fr.times(2));
+        assertEquals(Money.franc(15),fr.times(3));
+    }
+    @Test
+    public void testCurrency() {
+        assertEquals("USD",Money.dollar(1).currency());
+        assertEquals("CHF",Money.franc(1).currency());
+    }
+    @Test
+     public void testDifferentClassEquality() {
+        assertTrue(new Money(10,"CHF").equals(new Franc(10,"CHF")));
+        assertFalse(new Franc(15,"CHF").equals(new Franc(10,"CHF")));
+    }
+}
---
+++ src/money/Franc.java
@@ -0,0 +1,11 @@
+package money;
+
+public class Franc extends Money {
+
+    Franc(int amount,String currency) {
+        super(amount,currency);
+    }
+    /*Money times(int multiplier) {
+        return new Money(amount * multiplier,currency);
+    }*/
+}
