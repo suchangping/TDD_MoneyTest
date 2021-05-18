3be00628bc7b23bd4d5c74cbbb527f8f963d796e
10072002su_changping@cn.tre-inc.com <10072002su_changping@cn.tre-inc.com>
2021-05-18 11:05:15 +0900

    chapter15

--- src/money/Sum.java
+++ src/money/Sum.java
@@ -1,14 +1,19 @@
 package money;
 
 public class Sum implements Expression{
-    Money augend;
-    Money addend;
-    Sum(Money augend, Money addend) {
+    Expression augend;
+    Expression addend;
+    Sum(Expression augend, Expression addend) {
         this.augend = augend;
         this.addend = addend;
     }
     public Money reduce(Bank bank, String to) {
-        int amount = this.augend.amount + this.addend.amount;
+        int amount = this.augend.reduce(bank, to).amount
+                + this.addend.reduce(bank, to).amount;
         return new Money(amount, to);
+    }
+    @Override
+    public Expression plus(Expression addend) {
+        return null;
     }
 }
